29fb8e69f7e1973940ed6ef4affaf8cce6cc1693
claradiass <claragoncalves370@gmail.com>
2023-09-18 07:01:14 -0300

    alteração estudante

--- src/test/java/Estudante.java
+++ src/test/java/Estudante.java
@@ -1,15 +1,47 @@
 public class Estudante{
   String nome;
   int matricula ;
-  float nota1, nota2, nota3;
+  double n1, n2, n3;
 
-  float media(){
-    return (nota1 + nota2 + nota3)/3;
+  public Estudante(String nome, int matricula, double n1, double n2, double n3){
+    this.nome = nome;
+    this.matricula = matricula;
+    this.n1 = n1;
+    this.n2 = n2;
+    this.n3 = n3;
+  }
+
+  public double getMedia(){
+    return (n1 + n2 + n3)/3;
     
   };
 
+  public void setNotas(double n1, double n2, double n3){
+    this.n1 = n1;
+    this.n2 = n2;
+    this.n3 = n3;
+  }
+
+  double getNota1(){
+    return n1;
+  }
+
+  double getNota2(){
+    return n2;
+  }
+
+  double getNota3(){
+    return n3;
+  }
+
+  // Crie uma exceção NotaInvalidaException, que deve ser lançada quando uma nota negativa é passada para os métodos que recebem nota
+
+//   Crie um caso de teste para testar o método double getMedia()
+//   Crie um caso de teste para testar os métodos void setNotas(double n1, double n2, double n3), double getNota1(), double getNota2() e double getNota3()
+
+
   void resultado(){
-    float mediaf = media();
+    double mediaf = getMedia();
     if (mediaf >= 70f){
       System.out.println("Aluno aprovado");
     } else {
@@ -22,18 +54,18 @@ public class Estudante{
         Estudante estudante1 = new Estudante();
         estudante1.nome = "ana";
         estudante1.matricula = 20202020;
-        estudante1.nota1 = 10f;
-        estudante1.nota2 = 70f;
-        estudante1.nota3 = 20f;
+        estudante1.n1 = 10f;
+        estudante1.n2 = 70f;
+        estudante1.n3 = 20f;
       
         estudante1.resultado();
 
         Estudante estudante2 = new Estudante();
         estudante2.nome = "aline";
         estudante2.matricula = 20202021;
-        estudante2.nota1 = 70f;
-        estudante2.nota2 = 70f;
-        estudante2.nota3 = 80f;
+        estudante2.n1 = 70f;
+        estudante2.n2 = 70f;
+        estudante2.n3 = 80f;
       
         estudante2.resultado();
         
